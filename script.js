267ebf205343ac6b22583b784f9a766a69c85ac3
HariharansudhanM <hariharanskr98@gmail.com>
2024-04-10 17:43:07 +0530

    Upload

---
+++ script.js
@@ -0,0 +1,77 @@
+//Callback hell
+
+"use strict";
+
+const display = () => {
+  setTimeout(function changeCountdown() {
+    document.querySelector(".textArea").textContent = "9";
+    (function () {
+      setTimeout(function changeCountdown() {
+        document.querySelector(".textArea").textContent = "8";
+
+        (function () {
+          setTimeout(function changeCountdown() {
+            document.querySelector(".textArea").textContent = "7";
+
+            (function () {
+              setTimeout(function changeCountdown() {
+                document.querySelector(".textArea").textContent = "6";
+
+                (function () {
+                  setTimeout(function changeCountdown() {
+                    document.querySelector(".textArea").textContent = "5";
+
+                    (function () {
+                      setTimeout(function changeCountdown() {
+                        document.querySelector(".textArea").textContent = "4";
+
+                        (function () {
+                          setTimeout(function changeCountdown() {
+                            document.querySelector(".textArea").textContent =
+                              "3";
+
+                            (function () {
+                              setTimeout(function changeCountdown() {
+                                document.querySelector(
+                                  ".textArea"
+                                ).textContent = "2";
+
+                                (function () {
+                                  setTimeout(function changeCountdown() {
+                                    document.querySelector(
+                                      ".textArea"
+                                    ).textContent = "1";
+
+                                    (function () {
+                                      setTimeout(
+                                        function changeCountdown() {
+                                          document.querySelector(
+                                            ".textArea"
+                                          ).textContent =
+                                            "Happy Independence day";
+                                          return;
+                                        },
+
+                                        1000
+                                      );
+                                    })();
+                                  }, 1000);
+                                })();
+                              }, 1000);
+                            })();
+                          }, 1000);
+                        })();
+                      }, 1000);
+                    })();
+                  }, 1000);
+                })();
+              }, 1000);
+            })();
+          }, 1000);
+        })();
+      }, 1000);
+    })();
+  }, 1000);
+};
+
+display();
